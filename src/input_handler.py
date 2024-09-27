7363a8e45791783f13f1db3214f3f5f83856f9e5
sp8cky <jh4112000@gmx.de>
2024-09-27 16:37:00 +0200

    Input, Add and Delete from database

---
+++ src/input_handler.py
@@ -0,0 +1,19 @@
+import getpass
+
+def get_user_choice():
+    print("\nChoose an option:")
+    print("1. View entries")
+    print("2. Add entry")
+    print("3. Delete entry")
+    print("4. Delete all entries")
+    print("5. Exit")
+
+    choice = input("Enter your choice (1-5): ")
+    return choice
+
+def get_input():
+    website = input("Enter the name of the website: ")
+    username = input("Enter your username: ")
+    password = getpass.getpass("Enter your password: ")
+
+    return website, username, password
---
+++ src/password_manager.py
@@ -0,0 +1,48 @@
+from input_handler import get_user_choice, get_input
+from db_handler import create_table, add_entry, get_entries, delete_entry, delete_all_entries
+
+def main():
+    create_table() # create table (if not exists)
+    while True:
+        choice = get_user_choice()
+
+        if choice == '1': # View entries
+            entries = get_entries()
+            print("\nStored entries (Website, Username, Password):")
+            for index, entry in enumerate(entries):
+                print(f"{index + 1}. {entry[1]}, {entry[2]}, {entry[3]}")
+
+        elif choice == '2': # Add entry
+            website, username, password = get_input()
+            add_entry(website, username, password)
+            print("Entry added successfully!")
+
+        elif choice == '3': # Delete entry
+            entries = get_entries()
+            if not entries:
+                print("No entries to delete.")
+                continue
+            print("\nSelect an entry to delete:")
+            for index, entry in enumerate(entries):
+                print(f"{index + 1}. Website: {entry[1]}")
+
+            entry_index = int(input("Enter the number of the entry to delete: ")) - 1
+            if 0 <= entry_index < len(entries):
+                delete_entry(entries[entry_index][0])  # Using the entry ID for deletion
+                print("Entry deleted successfully!")
+            else:
+                print("Invalid entry number.")
+
+        elif choice == '4': # Delete all entries
+            delete_all_entries()
+            print("All entries deleted successfully!")
+
+        elif choice == '5':
+            print("Exiting the program.")
+            break
+
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
